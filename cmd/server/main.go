ccd0753ad4a1754f2c5aba8cad39ab9678c28b1b
Andrey Denisov <and161185@gmail.com>
2025-04-20 16:32:01 +0500

    inc1: server , update handler, in-memory storage

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/and161185/metrics-alerting/cmd/server/handlers"
+	"github.com/and161185/metrics-alerting/storage"
+)
+
+func main() {
+
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+
+	storage := storage.NewMemStorage()
+
+	http.HandleFunc(`/update/`, handlers.UpdateMetricHandler(storage))
+	return http.ListenAndServe(`:8080`, nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/and161185/metrics-alerting
+
+go 1.22.1
