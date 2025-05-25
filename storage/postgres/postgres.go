50dcb57f6878b8b1175e6baa08d6a553fb976d14
Andrey Denisov <and161185@gmail.com>
2025-05-25 20:04:46 +0500

    fix postgre storage

--- storage/postgres/postgres.go
+++ storage/postgres/postgres.go
@@ -4,11 +4,13 @@ import (
 	"context"
 
 	"github.com/and161185/metrics-alerting/model"
+	"github.com/and161185/metrics-alerting/storage/inmemory"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type PostgresStorage struct {
 	db *pgxpool.Pool
+	ms *inmemory.MemStorage
 }
 
 func NewPostgresStorage(DatabaseDsn string) (*PostgresStorage, error) {
@@ -16,27 +18,27 @@ func NewPostgresStorage(DatabaseDsn string) (*PostgresStorage, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &PostgresStorage{db: db}, nil
+	return &PostgresStorage{db: db, ms: inmemory.NewMemStorage()}, nil
 }
 
 func (store *PostgresStorage) Save(m *model.Metric) error {
-	return nil
+	return store.ms.Save(m)
 }
 
 func (store *PostgresStorage) Get(m *model.Metric) (*model.Metric, error) {
-	return nil, nil
+	return store.ms.Get(m)
 }
 
 func (store *PostgresStorage) GetAll() (map[string]*model.Metric, error) {
-	return nil, nil
+	return store.ms.GetAll()
 }
 
 func (store *PostgresStorage) SaveToFile(filePath string) error {
-	return nil
+	return store.ms.SaveToFile(filePath)
 }
 
 func (store *PostgresStorage) LoadFromFile(filePath string) error {
-	return nil
+	return store.ms.LoadFromFile(filePath)
 }
 
 func (store *PostgresStorage) Ping(ctx context.Context) error {
